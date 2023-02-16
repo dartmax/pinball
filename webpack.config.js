d685ea20c5b0ca27ed59e6be2e8c23deb2455153
dartmax <promostend@gmail.com>
2023-02-15 19:00:23 -0500

    TS0006: Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 module.exports = {
     devtool: "source-map",
     entry: {
-        main:path.resolve(__filename, './index.js'),
+        main:path.resolve(__dirname, './src'),
     },
     output: {
         path: path.resolve(__dirname, './dist'),
